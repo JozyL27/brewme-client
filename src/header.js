4b9f5307d0e21c1cda9f160674be66bfc8b32d20
Jose <jozylopez27@gmail.com>
2020-03-11 10:24:47 -0500

    added header functions, nav

--- src/header.js
+++ src/header.js
@@ -1,10 +1,48 @@
 import React from 'react'
+import TokenService from './services/token-service'
+import { Link } from 'react-router-dom'
 
 export default class Header extends React.Component {
+    handleLogoutClick = () => {
+        TokenService.clearAuthToken()
+    }
+
+    renderLogoutLink() {
+        return (
+            <div className='loggedIn'>
+                <Link
+                onClick={this.handleLogoutClick}
+                to='/'>
+                Logout
+                </Link>
+            </div>
+        )
+    }
+
+    renderLoginLink() {
+        return (
+            <div className='renderLogin'>
+                <Link
+                to='/login'>
+                Login
+                </Link>
+            </div>
+        )
+    }
+
     render() {
     return (
         <>
-        <h1>BrewMe.io</h1>
+        <nav>
+            <h1>
+                <Link to='/'>
+                Brewme.io
+                </Link>
+            </h1>
+            {TokenService.hasAuthToken()
+            ? this.renderLogoutLink()
+            : this.renderLoginLink()}
+        </nav>
         </>
     )
     }
