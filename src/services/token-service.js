d101893b9ab4688d5706f3c56ea1c28c242011cc
Jose <jozylopez27@gmail.com>
2020-03-13 11:54:34 -0500

    added parse auth func

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -16,6 +16,19 @@ const TokenService = {
     makeBasicAuthToken(userName, password) {
         return window.btoa(`${userName}:${password}`)
     },
+    parseAuthToken(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(c => {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    const payload = JSON.parse(jsonPayload);
+
+    return {
+      id: payload.user_id
+    }
+  },
 }
 
 
