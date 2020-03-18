be403301b36f3d51b4adaaa6f56c4140fdbe0928
Jose <jozylopez27@gmail.com>
2020-03-18 15:45:07 -0500

    added snapshot test to navbar

--- src/tests/NavBar.spec.js
+++ src/tests/NavBar.spec.js
@@ -2,12 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
 import NavBar from '../components/NavBar/NavBar'
+import { BrowserRouter } from 'react-router-dom'
 
 
 describe(`NavBar component`, () => {
+
     it(`renders without crashing`, () => {
         const div = document.createElement('div')
-        ReactDOM.render(<NavBar />, div)
+        ReactDOM.render(
+        <BrowserRouter>
+        <NavBar />
+        </BrowserRouter>
+        , div)
         ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it(`should render the UI as expected`, () => {
+        const tree = renderer
+            .create(
+            <BrowserRouter>
+            <NavBar />
+            </BrowserRouter>
+            )
+            .toJSON()
+            expect(tree).toMatchSnapshot()
     })
 })
