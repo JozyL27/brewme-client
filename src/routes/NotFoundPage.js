547753f92cdea5ad23a112793a6d5d49d8b92b43
Jose <jozylopez27@gmail.com>
2020-03-18 16:05:00 -0500

    added styling to pnf component

--- src/routes/NotFoundPage.js
+++ src/routes/NotFoundPage.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import './NotFoundPage.css'
 
 
 export default class NotFoundPage extends React.Component {
     render() {
         return (
-            <section>
-                <h2>404 - Page not found</h2>
-                <p>Try going back to your previous page.</p>
+            <section className="notFound">
+                <h2 className="notFoundHeader">404 - Page not found bro</h2>
+                <p className="notFoundPara">Try going back to your previous page dude</p>
             </section>
         )
     }
