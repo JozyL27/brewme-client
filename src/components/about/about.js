0c3b3d41af02c947193f0a7566e1f1e509e9b4d7
Jose <jozylopez27@gmail.com>
2020-03-20 14:23:41 -0500

    added contrasting background

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -5,7 +5,7 @@ export default class About extends React.Component {
     render() {
     return (
         <section className='about'>
-        <h3>About | How To</h3>
+        <h2>About | How To</h2>
         <p className="aboutPara">BrewMe's intended use is to help users save their favorite beers.
         Users are able to add and save their favorite beers to their "My Beers"
         list. Users are also able to search the beer database for beer info and use the
