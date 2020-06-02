fe15a62495605a6d0e8b70bf48619c6c55834d7c
Jose <jozylopez27@gmail.com>
2020-06-01 21:22:00 -0500

    updated about me

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -14,10 +14,13 @@ export default class About extends React.Component {
     return (
         <section className='about'>
         <h2>About | How To</h2>
-        <p className="aboutPara">BrewMe's intended use is to help users save their favorite beers.
-        Users are able to add and save their favorite beers to their "My Beers"
-        list. Users are also able to search the beer database for beer info and use the
-        "Random Beer" feature to retrieve a random beer from the database.
+        <p className="aboutPara">BrewMe is a database containing almost 5,000 beer entries.
+        Developers are able to use this  RESTful API to add and save their favorite beers to their "My Beers"
+        list. Developers are also able to search the beer database for beer info and use the
+        "Random Beer" feature to retrieve a random beer from the database. This API has authentication 
+        but is not required to search and / or retrieve a random beer from the database. API documentation
+        can be found <a href='https://github.com/JozyL27/brewme-server' className='server'> here</a>. You can demo the
+        API using this basic React App.
         {' '}
         {TokenService.hasAuthToken() ? null
         : <span className="loginInfo">
